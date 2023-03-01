5bec8034099c7ba9d99fcd1476dcc94e5fb0090f
katja <schultzkatja@gmx.de>
2023-03-01 16:24:36 +0100

    Add first module initialization calls

--- sketch_temperature_logger/sketch_temperature_logger.ino
+++ sketch_temperature_logger/sketch_temperature_logger.ino
@@ -8,12 +8,20 @@
 #include <SD.h>
 #include <SPI.h>
 #include <OneWire.h>
+#include <RTClib.h>
 
+RTC_DS1307 rtc; // to communicate with the clock
 
+OneWire ow(4); // hand over pin to one wire bus
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 
+  // initialize and check if modules running
+  if(!rtc.begin()) {
+    Serial.println("RTC is NOT running. Let's set the time now!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 }
 
 void loop() {
