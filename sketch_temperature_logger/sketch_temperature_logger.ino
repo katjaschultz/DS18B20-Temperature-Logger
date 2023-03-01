8a7cc19ab3856a9f212d9df06bffca2e552df2eb
katja <schultzkatja@gmx.de>
2023-03-01 16:08:17 +0100

    include arduino code

---
+++ sketch_temperature_logger/sketch_temperature_logger.ino
@@ -0,0 +1,22 @@
+/********************
+ * Author  Katja Schultz
+ * eMail: schultzkatja@gmx.de
+ * Date: 2023-01-03
+  */
+
+#include <OneWire.h>
+#include <SD.h>
+#include <SPI.h>
+#include <OneWire.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
