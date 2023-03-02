b1757da8a10aaac944807727dc1be73543a63f04
katja <schultzkatja@gmx.de>
2023-03-02 15:15:56 +0100

    complete main code to measure spot temperature

---
+++ sketch_temperature_logger/temp_logger_commands.h
@@ -0,0 +1,11 @@
+/*
+ * This file contains all the required HEX commands 
+ * for working with the temperature sensor
+ */
+
+
+#define CONVERT_T 0x44
+#define READ_ROM 0x33
+#define IS_DS18B20_sensor 0x28
+#define SKIP_ROM 0xCC
+#define READ_SCRATCHPAD 0xBE
